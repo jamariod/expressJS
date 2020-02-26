3b3e3fa863f4edd00459ae6a324c1d64b43f210d
jamariod <j25ghs99@aol.com>
2020-02-26 13:22:20 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+app.listen(3333, () => {
+  console.log("Server running on port 3333");
+});
+const rootController = require("./routes/index"),
+  catController = require("./routes/cat"),
+  dogController = require("./routes/dog"),
+  fooController = require("./routes/foo"),
+  personController = require("./routes/person");
+app.use("/", rootController);
+app.use("/cat", catController);
+app.use("/dog", dogController);
+app.use("/all", fooController);
+app.use("/person", personController);
